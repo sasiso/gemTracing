9b8578adc1b845535840af8af8dd3b676cae196d
sasiso <satbirsoni@gmail.com>
2024-04-28 09:23:58 +1000

    Lower the threshold

--- src/image_processor.py
+++ src/image_processor.py
@@ -9,7 +9,7 @@ def get_contours(img):
     gray = cv2.convertScaleAbs(gray)
     median_blur = cv2.medianBlur(gray, 5)
 
-    edges = cv2.Canny(median_blur, 100, 200)
+    edges = cv2.Canny(median_blur, 50, 200)
 
     ret, thresh = cv2.threshold(edges, 127, 255, 0)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
